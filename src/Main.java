a3cf2cb0698119c9fe99c917a311aa6904443648
umi283 <1149624963@qq.com>
2020-10-18 15:39:58 +0800

    '泛型堆栈'

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,48 @@
 import java.util.*;
 
+class MyStack<Item>{
+    private Node first;
+    private int n;
+    private class Node{
+        Item data;
+        Node next;
+    }
+
+    public void push(Item item){
+        Node old = first;
+        first = new Node();
+        first.data=item;
+        first.next=old;
+        n++;
+    }
+
+    public Item pop(){
+        Item item = first.data;
+        first=first.next;
+        n--;
+        return item;
+    }
+
+    public Item top(){
+        return first.data;
+    }
+
+    public boolean isEmpty(){
+        return n==0;
+    }
+
+    public int size(){
+        return n;
+    }
 
 
-public class Main {
+}
+
+public class Main{
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
+        MyStack<Integer> stack = new MyStack<Integer>();
+        stack.push(Integer.valueOf(1));
+        stack.push(Integer.valueOf(2));
+        stack.push(Integer.valueOf(3));
     }
 }
